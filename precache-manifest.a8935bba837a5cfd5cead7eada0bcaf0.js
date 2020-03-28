f6e2863eb4510dff1c6b953888d25493dc3cea0d
Carlos <carlos@alphabeta.io>
2020-03-28 18:28:55 -0300

    Updates

---
+++ precache-manifest.a8935bba837a5cfd5cead7eada0bcaf0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d581df9701c4323438d44dc6c74440ea",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "654552fc1b813e44efaf",
+    "url": "/pomodoro/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "a8a034c732bc610e6b30",
+    "url": "/pomodoro/static/css/main.9c5c5d05.chunk.css"
+  },
+  {
+    "revision": "654552fc1b813e44efaf",
+    "url": "/pomodoro/static/js/2.6fd251aa.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/pomodoro/static/js/2.6fd251aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a034c732bc610e6b30",
+    "url": "/pomodoro/static/js/main.347e30b9.chunk.js"
+  },
+  {
+    "revision": "d13b1f1c4e2fdd32784a",
+    "url": "/pomodoro/static/js/runtime-main.c0f3f903.js"
+  },
+  {
+    "revision": "f969cfae344d8cf1630053b6979f1a46",
+    "url": "/pomodoro/static/media/alarm_clock.f969cfae.mp3"
+  },
+  {
+    "revision": "ba023f0cae623de127b30e3eaca479b6",
+    "url": "/pomodoro/static/media/il_pomodoro.ba023f0c.png"
+  }
+]);
